05b95084f283ed9c0c6fbd227e52efbe1f9fb64d
Stanko Milosev <stanko.milosev@windowslive.com>
2020-01-26 12:21:38 +0100

    - readme updated - test connection to mySql

--- AllPics2gMaps/AllPics2gMaps/Controllers/ValuesController.cs
+++ AllPics2gMaps/AllPics2gMaps/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace AllPics2gMaps.Controllers
 {
@@ -14,7 +16,15 @@ namespace AllPics2gMaps.Controllers
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-      return new string[] { "value1", "value2" };
+      IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+      string connectionString = configuration["connectionString"];
+
+      using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+      {
+        mySqlConnection.Open();
+      }
+
+        return new string[] { $"value1", "value2" };
     }
 
     // GET api/values/5
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AllPics2gMaps
 Display every photo which I have on Google Maps. Since I want to show every single picture which I ever took on google map I need separate project from Pics2gMaps because loading is taking too much time. With this project I will try different approaches, to filter pictures depending on zoom level, radius in which I want to show pictures, countries, cities...
+
+Will be uploaded on: https://allpics2gmaps.azurewebsites.net
